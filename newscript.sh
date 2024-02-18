85f3fb0f1feb758bfba1f67036a389ad49bbe205
EfimPechersky <81502368+EfimPechersky@users.noreply.github.com>
2024-02-18 16:15:50 +0800

    Update newscript.sh

--- newscript.sh
+++ newscript.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Define the directory to search in and the days threshold
 directory="/home/efim/testdir"
 days_threshold=0
 extensions=(".txt" ".log" )
 found_paths=$(find "$directory" -type f)
-# Store the output of the find command in a variable
-echo "$extensions"
-# Iterate through the paths and remove each file
 for path in $found_paths; do
-    echo "$path"
     for ext in ${extensions[*]};do
 	if [[ $path = *$ext* ]];then
 	    rm "$path"
